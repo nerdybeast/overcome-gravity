25c68b7b962b2b78a564319e4c0db98abd04f3de
Michael Penrod <therealnerdybeast@gmail.com>
2018-10-30 10:45:44 -0500

    Added some code comments for dotenv.

--- config/dotenv.js
+++ config/dotenv.js
@@ -6,6 +6,8 @@ module.exports = function(env) {
 
 		// Fail build when there is missing any of clientAllowedKeys environment variables.
 		// By default false.
+		//If this value is set to true, the ember build will fail if it can't find a ".env" file in the root of this project
+		//which in theory, only exists when running locally. When deployed to a host like Heroku, this file will not exist in the root.
 		failOnMissingKey: env === 'local'
 	};
 };
