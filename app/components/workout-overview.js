8b700bcfbcc6f4565a55ebe3c81dbd77d8c77180
Michael Penrod <therealnerdybeast@gmail.com>
2018-11-16 17:40:19 -0600

    Workouts, exercises, and sets are now ordered.

--- app/components/workout-overview.js
+++ app/components/workout-overview.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import ComponentValidateMixin from 'overcome-gravity/mixins/component-validator-mixin';
-import { alias, sort } from '@ember/object/computed';
 
 export default Component.extend(ComponentValidateMixin, {
 
@@ -9,10 +8,5 @@ export default Component.extend(ComponentValidateMixin, {
 	init() {
 		this._super(...arguments);
 		this.validateArguments('workout-overview', ['workout']);
-	},
-
-	exercises: alias('workout.exercises'),
-
-	exercisesSortAsc: Object.freeze(['order']),
-	sortedExercises: sort('exercises', 'exercisesSortAsc'),
+	}
 });
