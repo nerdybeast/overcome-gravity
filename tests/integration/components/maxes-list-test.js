2f897b9a786387cb6f92af732fe5ae8ead2a3139
Michael Penrod <therealnerdybeast@gmail.com>
2019-03-29 16:58:24 -0500

    Ran codemods for 3.8.0

--- tests/integration/components/maxes-list-test.js
+++ tests/integration/components/maxes-list-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | maxes-list', function(hooks) {
 
     await render(hbs`{{maxes-list}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | maxes-list', function(hooks) {
       {{/maxes-list}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
--- tests/integration/components/page-heading-test.js
+++ tests/integration/components/page-heading-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | page-heading', function(hooks) {
 
     await render(hbs`{{page-heading}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | page-heading', function(hooks) {
       {{/page-heading}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
